5b9a8b8887937cd5f19d6c1413368904e244bcf0
Tom Dilatush <tom@dilatush.com>
2021-08-23 14:41:21 -0600

    Very basic initial structure, logging...

--- src/com/dilatush/networkmonitor/NetworkMonitor.java
+++ src/com/dilatush/networkmonitor/NetworkMonitor.java
@@ -1,7 +1,44 @@
 package com.dilatush.networkmonitor;
 
+import com.dilatush.util.ScheduledExecutor;
+
+import java.util.logging.Logger;
+
 /**
+ * The main class for the NetworkMonitor program.  This class is instantiated as a singleton.
+ *
  * @author Tom Dilatush  tom@dilatush.com
  */
 public class NetworkMonitor {
+
+    public static final NetworkMonitor INSTANCE = new NetworkMonitor();
+
+    private static final int SCHEDULER_THREADS = 6;  // number of threads in the shared scheduler's thread pool...
+
+    public final ScheduledExecutor   scheduledExecutor;
+
+    private final Logger             LOGGER;
+
+
+    public NetworkMonitor() {
+
+        // set the configuration file location (must do before any logging actions occur)...
+        System.getProperties().setProperty( "java.util.logging.config.file", "logging.properties" );
+        LOGGER = Logger.getLogger( new Object(){}.getClass().getEnclosingClass().getSimpleName() );
+
+        // set up a scheduled executor service for all to use...
+        scheduledExecutor = new ScheduledExecutor( SCHEDULER_THREADS, false );
+
+    }
+
+
+    private void run() {
+
+        LOGGER.info( "NetworkMonitor is starting..." );
+    }
+
+
+    public static void main( final String[] _args ) {
+        INSTANCE.run();
+    }
 }
